3fcced6921ce2132f8a4edcb7d0b080829e1f23e
Efe Dogruoz <edogruoz@cs.uchicago.edu>
2021-02-14 13:20:16 -0600

    python file

---
+++ cscc/cscc.py
@@ -0,0 +1,25 @@
+# CSCC Project
+#
+# CMSC 12200
+#
+# Efe Dogruoz, Ebru Ermis, Mey Abdullahoglu, Kevin Ramirez
+
+"""
+Tasks:
+    write code that takes in model and use, and returns emission + spending
+    write code that compares emission with average and gives rec for hours to 
+        cut down
+    write code to rec. (alternative cars OR public transport) and gives info 
+        about savings
+    translate the written code to user interface
+"""
+
+def get_data(url):
+    pass
+
+def compare_emission(data):
+    pass
+
+def get_recommendation(data):
+    pass
+
