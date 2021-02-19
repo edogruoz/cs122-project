413ef5d34162dc7c802490061c2e7ce31bc6b7e7
Kevin Ramirez <kevinramirez@cs.uchicago.edu>
2021-02-19 16:18:19 -0600

    restructured method by which csv is accepted and saved to db

--- cscc/cscc.py
+++ cscc/cscc.py
@@ -13,35 +13,24 @@ Tasks:
         about savings
     translate the written code to user interface
 """
+
 import pandas as pd
 import sqlite3
-import subprocess
-from urllib.request import urlopen
+import questionary
 
 URL = "https://www.fueleconomy.gov/feg/epadata/vehicles.csv"
 
-# Year bounds on dataset to reject certain user input
-YEAR_LB, YEAR_UB = 1980, 2025
-# Widens user search so they don't need exact year
-YEAR_TOLERANCE = 5
+PERTINENT_DATA = ['id', 'make', 'model', 'year', 'VClass',
+                  'pv2', 'pv4', 'hpv', 'lv2', 'lv4', 'hlv',
+                  'fuelCost08', 'fuelCostA08', 'fuelType',
+                  'co2TailpipeGpm', 'co2TailpipeAGpm']
 
 MILES_PER_HOUR = 30 #needs to be edited
 SELECT_CMD = ("SELECT co2TailpipeGpm, fuelCost08,"
               " fuelCostA08, fuelType FROM vehicles ")
 WHERE_CMD = "WHERE make = ? AND model = ? AND year = ?"
 
-def get_data(url):
-    response = urlopen(url)
-    data = response.read()
-    filename = url.split('/')[-1]
-    
-    with open(filename, 'wb') as csv_file:
-        csv_file.write(data)
-    
-    return filename
 
-# POSSIBLE REPLACEMENT FOR TRIM CSV
-# PENDING CONSIDERATION
 def build_db(connection):
     '''
     Creates sqlite database file containing only the columns
@@ -52,69 +41,9 @@ def build_db(connection):
             superfluous columns
         connection: connection object for db file
     '''
-    pertinent_data = ['id', 'make', 'model', 'year', 'VClass',
-                      'pv2', 'pv4', 'hpv', 'lv2', 'lv4', 'hlv',
-                      'fuelCost08', 'fuelCostA08', 'fuelType',
-                      'co2TailpipeGpm', 'co2TailpipeAGpm']
-    df = pd.read_csv(URL, usecols=pertinent_data)
-    df.set_index('id', drop=True, inplace=True)
+    df = pd.read_csv(URL, usecols=PERTINENT_DATA, index_col='id', engine='c')
     # IMPORTANT .to_sql cannot create a table with a primary key
     df.to_sql('vehicles', con=connection, if_exists='replace')
-
-def trim_csv(full_data):
-    '''
-    Creates lightweight csv containing only the columns
-    that will actually see use from the original vehicle csv.
-
-    Input: full_data (csv) original data set holding
-        superfluous columns
-    
-    Returns: new csv file containing only specific columns
-        to feed into our sqlite table
-    '''
-    pertinent_data = ['id', 'make', 'model', 'year', 'VClass',
-                      'pv2', 'pv4', 'hpv', 'lv2', 'lv4', 'hlv',
-                      'fuelCost08', 'fuelCostA08', 'fuelType',
-                      'co2TailpipeGpm', 'co2TailpipeAGpm']
-    df = pd.read_csv(full_data, usecols=pertinent_data)
-    df.to_csv('trimmed.csv', index=False)
-
-def csv_to_sql():
-    database = "cscc.db"
-    conn = sqlite3.connect(database)
-    c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS vehicles (CO2_A REAL, CO2 REAL,"
-              " fuel_cost INTEGER, fuel_cost_A INTEGER,"
-              " fuel_type varchar(200), hlv INTEGER, hpv INTEGER, id INTEGER,"
-              " lv2 INTEGER, lv4 INTEGER, make varchar(200),"
-              " model varchar(200), pv2 INTEGER, pv4 INTEGER,"
-              " v_class varchar(200), year INTEGER)")
-
-    subprocess.call(["sqlite3", database, ".separator ','",
-                     ".import trimmed.csv vehicles"])
-    
-    return conn
-
-def year_input_helper():
-    '''
-    Repeatedly asks user for input regarding car production
-    year. Will not accept if year is not an integer or year
-    given is not within a certain bound of the dataset.
-
-    Returns: year - user inputted value for their car
-    '''
-    while True:
-        try:
-            year = input("Enter car's production year "
-                         "(Optional, but recommended): ")
-            if not year:
-                return year
-            year = int(year)
-            if (YEAR_LB <= year <= YEAR_UB):
-                return year
-            print("Value is not within data bounds. Try again.")
-        except ValueError:
-            print("Value is not an integer. Try again.")
 
 # WIP
 def get_user_input(cursor):
@@ -122,42 +51,19 @@ def get_user_input(cursor):
     Creates a dictionary with user car information
     needed for look up as well as daily miles estimation
     '''
-    # Saves user input
-    make = input("Enter car's make: ")
-    while not make:
-        print("This field is required. Try again.")
-        make = input("Enter car's make: ")
-    model = input("Enter car's model (Optional, but recommended): ")
-    year = year_input_helper()
-    
-    # If input doesn't match database, builds query based on
-    # filled fields returning likely intended candidates
-    required = 'SELECT make, model, year FROM vehicles WHERE make LIKE ?'
-    model_option = ''
-    year_option = ''
-    params = ('%' + make + '%',)
-    if model:
-        model_option = ' AND model LIKE ?'
-        params += ('%' + model + '%',)
-    if year:
-        year_option = ' AND year BETWEEN ? AND ?'
-        lb, ub = year - YEAR_TOLERANCE, year + YEAR_TOLERANCE
-        params += (lb, ub)
-    query = required + model_option + year_option
+    make_query = 'SELECT DISTINCT make FROM vehicles'
+    make_results = cursor.execute(make_query).fetchall()
+    make_results = [i[0] for i in make_results]
+    #'SELECT EXISTS(SELECT 1 FROM myTbl WHERE u_tag="tag")'
+    make_ans = questionary.autocomplete("What is your car's make?\n",
+                                        choices=make_results).ask()
 
-    # Displays candidates in readable format for future selection
-    print("Now displaying results 10 at a time.")
-    next = ''
-    cursor.execute(query, params)
-    results = True
-    i = 1
-    while next != 'Q' and results:
-        results = cursor.fetchmany(10)
-        for row in results:
-            print(row)
-        print("Page " + str(i))
-        next = input("Enter Q to stop or any other key to continue: ")
-        i += 1
+    mod_year_query = 'SELECT DISTINCT model, year FROM vehicles WHERE make = ?'
+    mod_year_results = cursor.execute(mod_year_query, (make_ans,)).fetchall()
+    mod_year_results = [' '.join((i, str(j))) for i, j in mod_year_results]
+    mod_year_ans = questionary.autocomplete('What about model and year?\n',
+                                            choices=mod_year_results).ask()
+    print(' '.join([make_ans, mod_year_ans]))
 
 def get_emissions(input_dict, vehicles):
     model = input_dict["model"]
@@ -196,13 +102,18 @@ def go():
     of necessary milage reduction, or potential new car
     purchases/(public transportation use)
     '''
-    full_csv = get_data(URL)
-    trim_csv(full_csv)
-    conn = csv_to_sql()
-    #cscc_db = sqlite3.connect('cscc.db')
-    #build_db(cscc_db)
-    c = conn.cursor()
-    get_user_input(c)
+    # Creates database if none already exists, skips this
+    # computationally expensive processes otherwise.
+    try:
+        conn = sqlite3.connect('file:cscc.db?mode=rw', uri=True)
+    except sqlite3.OperationalError:
+        print('Local Database not found\n'
+              'Creating database...')
+        conn = sqlite3.connect('cscc.db')
+        build_db(conn)
+    cursor = conn.cursor()
+    get_user_input(cursor)
+    cursor.close()
     conn.close()
 
 if __name__ == "__main__":
